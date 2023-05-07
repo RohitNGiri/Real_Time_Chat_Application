c9fe4e6220a4f373dba04fbf201f12b53c9ac25e
RohitNGiri <procoderpc@gmail.com>
2023-05-07 12:16:07 +0530

    Complete overall funtionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,12 +22,12 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">Rg Chat</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <button>Sign in</button>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign In</button>
           {err && <span>Something went wrong</span>}
         </form>
         <p>You don't have an account? <Link to="/register">Register</Link></p>
